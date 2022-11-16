37253c25439936c24ffb3bf66d481d18aa41c054
jhnlambert <jhnlambert171@gmail.com>
2022-11-15 19:18:55 -0600

    initial commit
    base mechanium drive

---
+++ src/main/java/org/team5148/chargedup/Robot.java
@@ -0,0 +1,55 @@
+package org.team5148.chargedup;
+
+import edu.wpi.first.wpilibj.TimedRobot;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import edu.wpi.first.wpilibj.XboxController;
+
+
+import edu.wpi.first.wpilibj.drive.MecanumDrive;
+
+
+public class Robot extends TimedRobot {
+
+	//defining motor controlers 
+	CANSparkMax frontLeft = new CANSparkMax(1, MotorType.kBrushless);
+	CANSparkMax frontRight = new CANSparkMax(3, MotorType.kBrushless);
+	CANSparkMax backLeft = new CANSparkMax(2, MotorType.kBrushless);
+	CANSparkMax backRight = new CANSparkMax(4, MotorType.kBrushless);
+
+	//xbox controller
+	XboxController driveController = new XboxController(0);
+
+	//define a drive
+	MecanumDrive MDrive = new MecanumDrive(frontLeft , backLeft , frontRight , backRight);
+
+	@Override
+	public void autonomousInit() {
+
+	}
+
+	@Override
+	public void autonomousPeriodic() {
+
+	}
+
+	@Override
+	public void teleopInit() {
+		frontRight.setInverted(true);
+		backRight.setInverted(true);
+		
+	}
+
+	@Override
+	public void teleopPeriodic() {
+		//use controller input to set drive system
+		Double ySpeed = driveController.getLeftY();
+		Double xSpeed = driveController.getLeftX();
+		Double zRotation = driveController.getRightX(); 
+		MDrive.driveCartesian(ySpeed, xSpeed, zRotation);
+	
+		
+	}
+}
+
+
